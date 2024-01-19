cbbf6f68e9dc47484fa24daec583759caf28eb6b
Derian Marchan <derianmarchan@gmail.com>
2024-01-19 12:48:59 -0500

    Update contract-view component and item-view-template component

--- src/app/modules/tracking/contracts/pages/contract-view/contract-view.component.ts
+++ src/app/modules/tracking/contracts/pages/contract-view/contract-view.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ItemViewTemplateComponent } from '@component/item-view-template/item-view-template.component';
+import { Contract } from '@interface/contract';
+import { ItemViewConfiguration } from '@interface/itemView';
 
 @Component({
   selector: 'app-contract-view',
   standalone: true,
-  imports: [],
+  imports: [
+    ItemViewTemplateComponent,
+  ],
   templateUrl: './contract-view.component.html',
   styleUrl: './contract-view.component.scss'
 })
 export class ContractViewComponent {
-
+  public configuration: ItemViewConfiguration<Contract> = {
+    titleModule: 'Contrato',
+    itemPathServer: 'contract',
+    queryParams: { relations: 'client,clientBusinessUnit,plan' },
+    nameItemFn: (item) => `${item.code} - ${item.client?.name}`,
+    links: []
+  }
 }
