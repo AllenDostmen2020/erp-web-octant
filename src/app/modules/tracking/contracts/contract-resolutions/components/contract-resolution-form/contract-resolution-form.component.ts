fcaa59078c07f3e3e633fe9632af4a9118619115
Derian Marchan <derianmarchan@gmail.com>
2024-01-19 13:32:12 -0500

    Refactor contract resolution form component

--- src/app/modules/tracking/contracts/contract-resolutions/components/contract-resolution-form/contract-resolution-form.component.ts
+++ src/app/modules/tracking/contracts/contract-resolutions/components/contract-resolution-form/contract-resolution-form.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { InputAutocompleteTemplateComponent } from '@component/input-autocomplete-template/input-autocomplete-template.component';
+import { InputSelectTemplateComponent } from '@component/input-select-template/input-select-template.component';
 import { ContractResolutionEntityEnum, ContractResolutionTypeEnum } from '@interface/contractResolution';
 import { FirstLetterUppercasePipe } from '@pipe/first-letter-uppercase.pipe';
 import { NameModuleDatabase } from '@service/database-storage.service';
@@ -18,6 +20,8 @@ import { NameModuleDatabase } from '@service/database-storage.service';
     MatInputModule,
     FirstLetterUppercasePipe,
     MatSelectModule,
+    InputSelectTemplateComponent,
+    InputAutocompleteTemplateComponent,
   ],
 })
 export class ContractResolutionFormComponent {
@@ -29,5 +33,8 @@ export class ContractResolutionFormComponent {
 
   get typeCtrl(): FormControl {
     return this.form.get('type') as FormControl;
+  }
+  get responsibleUserIdCtrl(): FormControl {
+    return this.form.get('responsible_user_id') as FormControl;
   }
 }
