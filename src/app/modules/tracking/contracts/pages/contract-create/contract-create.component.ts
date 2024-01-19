a8c55aa1a561e9ac4841decd94b6e7629bec8cdf
Derian Marchan <derianmarchan@gmail.com>
2024-01-19 11:46:21 -0500

    Refactor code to improve performance and readability

---
+++ src/app/modules/tracking/contracts/pages/contract-create/contract-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './contract-create.component.html',
+  styleUrl: './contract-create.component.scss'
+})
+export class ContractCreateComponent {
+
+}
---
+++ src/app/modules/tracking/contracts/pages/contract-view/contract-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './contract-view.component.html',
+  styleUrl: './contract-view.component.scss'
+})
+export class ContractViewComponent {
+
+}
