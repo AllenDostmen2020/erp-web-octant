9d3814840fd7fc854c8b888bb0851dc6583da564
Derian Marchan <derianmarchan@gmail.com>
2024-02-12 11:31:01 -0500

    Update environment and configuration settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000/api/v1',
-  PUBLIC_PATH_SERVER: 'http://localhost:3000/',
-  URL_FILES_SERVER: 'http://localhost:3000/storage/',
+  API_URL: 'https://nuevo-octant-erp-production.up.railway.app/api/v1',
+  PUBLIC_PATH_SERVER: 'https://nuevo-octant-erp-production.up.railway.app/',
+  URL_FILES_SERVER: 'https://erp-allendostmen-application.s3.amazonaws.com/',
 };
