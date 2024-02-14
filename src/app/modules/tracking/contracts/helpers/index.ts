ddb3b7c92da1244374940c831b427a793f8c3ac7
Milton Marchan <marchanadrian2@gmail.com>
2024-02-14 11:19:32 -0500

    update forms client-contact and contract

--- src/app/modules/tracking/contracts/helpers/index.ts
+++ src/app/modules/tracking/contracts/helpers/index.ts
@@ -3,83 +3,85 @@ import { dateColumn, itemCreatedAtColumn, itemStatusColumn, itemUpdatedAtColumn,
 import { Contract, RecurrentTypeEnum } from "@interface/contract";
 
 export const getContractFormGroup = (): FormGroup => {
-  return new FormGroup({
-    client_business_unit_id: new FormControl(null, [Validators.required]),
+    return new FormGroup({
+        client_business_unit_id: new FormControl(null, [Validators.required]),
 
-    client_id: new FormControl(null, [Validators.required]),
-    client: new FormControl(null),
+        client_id: new FormControl(null, [Validators.required]),
+        client: new FormControl(null),
 
-    plan_id: new FormControl(null, [Validators.required]),
-    plan: new FormControl(null),
+        plan_id: new FormControl(null, [Validators.required]),
+        plan: new FormControl(null),
 
-    installation_date: new FormControl(new Date(), [Validators.required]),
-    start_date: new FormControl({ value: null, disabled: true }),
-    end_date: new FormControl({ value: null, disabled: true }),
-    period: new FormControl(null, [Validators.required]),
-    buy_price: new FormControl('0.00', [Validators.required]),
-    sale_price: new FormControl('0.00', [Validators.required]),
-    installation_price: new FormControl('0.00', [Validators.required]),
-    total_installation_price: new FormControl(null),
-    quantity: new FormControl(null, { validators: [Validators.required], updateOn: 'blur' }),
-    proration_days: new FormControl({ value: null, disabled: true }),
-    recurrent_type: new FormControl(RecurrentTypeEnum.MENSUAL, [Validators.required]),
+        installation_date: new FormControl(new Date(), [Validators.required]),
+        start_date: new FormControl({ value: null, disabled: true }),
+        end_date: new FormControl({ value: null, disabled: true }),
+        period: new FormControl(null, [Validators.required]),
+        buy_price: new FormControl('0.00', [Validators.required]),
+        sale_price: new FormControl('0.00', [Validators.required]),
+        installation_price: new FormControl('0.00', [Validators.required]),
+        total_installation_price: new FormControl(null),
+        quantity: new FormControl(null, { validators: [Validators.required], updateOn: 'blur' }),
+        proration_days: new FormControl({ value: null, disabled: true }),
+        recurrent_type: new FormControl(RecurrentTypeEnum.MENSUAL, [Validators.required]),
 
-    client_responsible_document_number: new FormControl(null),
-    client_responsible_document_type: new FormControl(null),
-    client_responsible_name: new FormControl(null),
-    client_responsible_phone: new FormControl(null),
-    client_responsible_email: new FormControl(null),
+        client_responsible_document_number: new FormControl(null),
+        client_responsible_document_type: new FormControl(null),
+        client_responsible_name: new FormControl(null),
+        client_responsible_paternal_name: new FormControl(null),
+        client_responsible_maternal_name: new FormControl(null),
+        client_responsible_phone: new FormControl(null),
+        client_responsible_email: new FormControl(null),
 
-    sale_user_id: new FormControl(null, [Validators.required]),
+        sale_user_id: new FormControl(null, [Validators.required]),
 
-    contract_vehicles: new FormArray([]),
-  })
+        contract_vehicles: new FormArray([]),
+    })
 }
 
 export const contractColumnsList = () => [
-  textColumn<Contract>({
-    title: 'Código',
-    sort: { key: 'code' },
-    displayValueFn: (item) => item.code,
-    routerLinkValue: { url: (item) => `/tracking/contract/view/${item.id}` },
-  }),
-  uppercaseColumn<Contract>({
-    title: 'Cliente/Unidad de negocio',
-    displayValueFn: (item) => item.client?.name,
-    displayAdditionalValueFn: (item) => item.client_business_unit?.name,
-    gridColumn: '1fr',
-    routerLinkValue: { url: (item) => `/organization/client/view/${item.client?.id}` },
-  }),
-  uppercaseColumn<Contract>({
-    title: 'Plan',
-    displayValueFn: (item) => item.plan?.name,
-  }),
-  uppercaseColumn<Contract>({
-    title: 'T. Periodo',
-    displayValueFn: (item) => item.recurrent_type,
-  }),
-  textColumn<Contract>({
-    title: 'Periodo',
-    displayValueFn: (item) => item.period,
-  }),
-  numberColumn<Contract>({
-    title: 'Unidades',
-    displayValueFn: (item) => item.quantity,
-    numberFormat: '2.0-0',
-  }),
-  numberColumn<Contract>({
-    title: 'Precio Und.',
-    displayValueFn: (item) => item.sale_price,
-  }),
-  dateColumn<Contract>({
-    title: 'Fecha inicio',
-    displayValueFn: (item) => item.start_date,
-  }),
-  dateColumn<Contract>({
-    title: 'Fecha fin',
-    displayValueFn: (item) => item.end_date,
-  }),
-  itemCreatedAtColumn(),
-  itemUpdatedAtColumn(),
-  itemStatusColumn(),
+    textColumn<Contract>({
+        title: 'Código',
+        sort: { key: 'code' },
+        displayValueFn: (item) => item.code,
+        routerLinkValue: { url: (item) => `/tracking/contract/view/${item.id}` },
+    }),
+    uppercaseColumn<Contract>({
+        title: 'Cliente/Unidad de negocio',
+        displayValueFn: (item) => item.client?.name,
+        displayAdditionalValueFn: (item) => item.client_business_unit?.name,
+        gridColumn: '1fr',
+        routerLinkValue: { url: (item) => `/organization/client/view/${item.client?.id}` },
+    }),
+    uppercaseColumn<Contract>({
+        title: 'Plan',
+        displayValueFn: (item) => item.plan?.name,
+    }),
+    uppercaseColumn<Contract>({
+        title: 'T. Periodo',
+        displayValueFn: (item) => item.recurrent_type,
+    }),
+    textColumn<Contract>({
+        title: 'Periodo',
+        displayValueFn: (item) => item.period,
+    }),
+    numberColumn<Contract>({
+        title: 'Unidades',
+        displayValueFn: (item) => item.quantity,
+        numberFormat: '2.0-0',
+    }),
+    numberColumn<Contract>({
+        title: 'Precio Und.',
+        displayValueFn: (item) => item.sale_price,
+    }),
+    dateColumn<Contract>({
+        title: 'Fecha inicio',
+        displayValueFn: (item) => item.start_date,
+    }),
+    dateColumn<Contract>({
+        title: 'Fecha fin',
+        displayValueFn: (item) => item.end_date,
+    }),
+    itemCreatedAtColumn(),
+    itemUpdatedAtColumn(),
+    itemStatusColumn(),
 ]
