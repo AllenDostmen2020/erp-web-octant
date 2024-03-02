b19a3bf0c6dbb56be9f2f40fd8c9e7e25b5f5077
Milton Marchan <marchanadrian2@gmail.com>
2024-03-02 12:22:07 -0500

    ------------------

--- src/app/modules/organization/clients/client-documents/components/client-document-payment-form/client-document-payment-form.component.ts
+++ src/app/modules/organization/clients/client-documents/components/client-document-payment-form/client-document-payment-form.component.ts
@@ -1,27 +1,59 @@
-import { Component, WritableSignal, inject, signal } from '@angular/core';
+import { Component, ViewEncapsulation, WritableSignal, inject, signal } from '@angular/core';
 import { Document } from '@interface/document';
 import { FetchService } from '@service/fetch.service';
 import { PaginatorData } from '@interface/paginator';
+import { AsyncPipe } from '@angular/common';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client-document-payment-form',
   standalone: true,
+  imports: [
+    AsyncPipe,
+    CdkDropList,
+    CdkDrag,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './client-document-payment-form.component.html',
-  styleUrl: './client-document-payment-form.component.scss'
+  styleUrl: './client-document-payment-form.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class ClientDocumentPaymentFormComponent {
   private fetch = inject(FetchService);
+  private activatedRoute = inject(ActivatedRoute);
   // public documents = toSignal(from(this.fetch.get<PaginatorData<Document>>('document')).pipe(map(data => data.data)));
   public documents: WritableSignal<Document[]> = signal([]);;
   public documentsToPay: WritableSignal<Document[]> = signal([]);
 
 
+  public listFrom: Document[] = [];
+  public listTo:Document[] = [];
 
   ngOnInit() {
     this.getDocuments();
   }
 
   private async getDocuments() {
-    this.documents.set((await this.fetch.get<PaginatorData<Document>>('document')).data);
+    const clientId = this.activatedRoute.snapshot.parent?.paramMap.get('id');
+    this.documents.set((await this.fetch.get<PaginatorData<Document>>(`document?relations=documentItems&client_id=${clientId}`)).data);
+    this.listFrom = this.documents();
   }
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        console.log('1: ',this.listTo);
+
+    } else {
+        transferArrayItem(
+            event.previousContainer.data,
+            event.container.data,
+            event.previousIndex,
+            event.currentIndex,
+        );
+    }
+}
 }
