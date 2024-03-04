876048f26c9e17232164e42adb17ba2b6e47351b
Milton Marchan <marchanadrian2@gmail.com>
2024-03-04 12:51:28 -0500

    sumItems Footer

--- src/app/modules/organization/clients/client-documents/components/client-document-payment-form/client-document-payment-form.component.ts
+++ src/app/modules/organization/clients/client-documents/components/client-document-payment-form/client-document-payment-form.component.ts
@@ -1,51 +1,80 @@
-import { Component, ViewEncapsulation, WritableSignal, inject, signal } from '@angular/core';
+import { Component, WritableSignal, inject, signal } from '@angular/core';
 import { Document } from '@interface/document';
 import { FetchService } from '@service/fetch.service';
 import { PaginatorData } from '@interface/paginator';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DecimalPipe } from '@angular/common';
 import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-client-document-payment-form',
-  standalone: true,
-  imports: [
-    AsyncPipe,
-    CdkDropList,
-    CdkDrag,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
-  templateUrl: './client-document-payment-form.component.html',
-  styleUrl: './client-document-payment-form.component.scss',
+    selector: 'app-client-document-payment-form',
+    standalone: true,
+    imports: [
+        AsyncPipe,
+        CdkDropList,
+        CdkDrag,
+        DecimalPipe
+    ],
+    templateUrl: './client-document-payment-form.component.html',
+    styleUrl: './client-document-payment-form.component.scss',
 })
 export class ClientDocumentPaymentFormComponent {
-  private fetch = inject(FetchService);
-  private activatedRoute = inject(ActivatedRoute);
-  // public documents = toSignal(from(this.fetch.get<PaginatorData<Document>>('document')).pipe(map(data => data.data)));
-  public documents: WritableSignal<Document[]> = signal([]);
-  public documentsToPay: WritableSignal<Document[]> = signal([]);
+    private fetch = inject(FetchService);
+    private activatedRoute = inject(ActivatedRoute);
+    // public documents = toSignal(from(this.fetch.get<PaginatorData<Document>>('document')).pipe(map(data => data.data)));
+    public documents: WritableSignal<Document[]> = signal([]);
+    public documentsToPay: WritableSignal<Document[]> = signal([]);
 
-  ngOnInit() {
-    this.getDocuments();
-  }
+    public sumSubtotal: number = 0;
+    public sumIgv: number = 0;
+    public sumSubtotalInit: number = 0;
+    public sumIgvInit: number = 0;
 
-  private async getDocuments() {
-    const clientId = this.activatedRoute.snapshot.parent?.paramMap.get('id');
-    this.documents.set((await this.fetch.get<PaginatorData<Document>>(`document?relations=documentItems&client_id=${clientId}`)).data);
-  }
-
-  drop(event: CdkDragDrop<any[]>) {
-    if (event.previousContainer === event.container) {
-        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-        transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex,
-        );
+    ngOnInit() {
+        this.getDocuments();
     }
-}
+    ngOnChanges() {
+        this.sumItems()
+        this.sumItemsInit();
+    }
+
+    private async getDocuments() {
+        const clientId = this.activatedRoute.snapshot.parent?.paramMap.get('id');
+        this.documents.set((await this.fetch.get<PaginatorData<Document>>(`document?relations=documentItems&client_id=${clientId}`)).data);
+        this.sumItemsInit();
+    }
+
+    drop(event: CdkDragDrop<any[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(
+                event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex,
+            );
+            this.sumItems();
+            this.sumItemsInit();
+        }
+    }
+
+    public sumItemsInit() {
+        this.sumSubtotalInit = (this.documents() ?? []).reduce((previousValue, item) => {
+            return previousValue + Number(item.total_value)
+        }, 0);
+        this.sumIgvInit = (this.documents() ?? []).reduce((previousValue, item) => {
+            return previousValue + Number(item.total_taxes)
+        }, 0);
+
+    }
+    public sumItems() {
+        this.sumSubtotal = (this.documentsToPay() ?? []).reduce((previousValue, item) => {
+            return previousValue + Number(item.total_value)
+        }, 0);
+        this.sumIgv = (this.documentsToPay() ?? []).reduce((previousValue, item) => {
+            return previousValue + Number(item.total_taxes)
+        }, 0);
+
+    }
 }
