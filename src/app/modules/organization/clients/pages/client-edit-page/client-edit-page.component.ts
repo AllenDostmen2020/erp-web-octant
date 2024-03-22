6527a77a5948f4f1b3e8fbae5788c837c55791dc
Milton Marchan <marchanadrian2@gmail.com>
2024-03-22 11:17:35 -0500

    -----

--- src/app/modules/organization/clients/pages/client-edit-page/client-edit-page.component.ts
+++ src/app/modules/organization/clients/pages/client-edit-page/client-edit-page.component.ts
@@ -2,16 +2,17 @@ import { Component, inject } from '@angular/core';
 import { ClientCreatePageComponent } from '../client-create-page/client-create-page.component';
 import { ActivatedRoute } from '@angular/router';
 import { ItemFormTemplateComponent } from '@component/item-form-template/item-form-template.component';
+import { FormControl } from '@angular/forms';
+import { ClientFormComponent } from '../../components/client-form/client-form.component';
 
 @Component({
   selector: 'app-client-edit-page',
   standalone: true,
-  imports: [ItemFormTemplateComponent],
+  imports: [ItemFormTemplateComponent, ClientFormComponent],
   templateUrl: './client-edit-page.component.html',
   styleUrl: './client-edit-page.component.scss'
 })
 export class ClientEditPageComponent extends ClientCreatePageComponent{
-    private activatedRoute = inject(ActivatedRoute);
 
     constructor(
     ) {
@@ -20,7 +21,17 @@ export class ClientEditPageComponent extends ClientCreatePageComponent{
         this.configuration.hiddeFields = true;
     }
 
-    override ngOnInit(): void {
+    get documentTypeCtrl(): FormControl {
+      return this.configuration.formGroup.get('document_type') as FormControl;
+    }
+    get documentNumberCtrl(): FormControl {
+      return this.configuration.formGroup.get('document_number') as FormControl;
+    }
+    get nameCtrl(): FormControl {
+      return this.configuration.formGroup.get('name') as FormControl;
+    }
+
+    ngOnInit(): void {
       this.documentTypeCtrl.disable();
       this.documentNumberCtrl.disable();
       this.nameCtrl.disable();
