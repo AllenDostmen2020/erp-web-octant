3d00d1a49af628c2826b7621ed747f2b612a6f40
Milton Marchan <marchanadrian2@gmail.com>
2024-04-01 13:23:24 -0500

    -----------

--- src/app/modules/organization/clients/pages/client-view-page/client-view-page.component.ts
+++ src/app/modules/organization/clients/pages/client-view-page/client-view-page.component.ts
@@ -39,7 +39,7 @@ export class ClientViewPageComponent {
             },
             {
                 routerLink: './document',
-                text: 'Documentos',
+                text: 'Facturas',
             },
             {
                 routerLink: './vehicle',
