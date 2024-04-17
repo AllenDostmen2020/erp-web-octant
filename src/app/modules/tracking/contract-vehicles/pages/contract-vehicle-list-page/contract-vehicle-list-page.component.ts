95350cf22558f1533a8c2d11cdf5ef315f0345b3
derianmarchan <derianmarchan@gmail.com>
2024-04-17 11:37:02 -0500

    Actualizar estilos en componentes contract-plan-form y contract-form

--- src/app/modules/tracking/contract-vehicles/pages/contract-vehicle-list-page/contract-vehicle-list-page.component.ts
+++ src/app/modules/tracking/contract-vehicles/pages/contract-vehicle-list-page/contract-vehicle-list-page.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, WritableSignal, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ItemListConfiguration, ItemListTemplateComponent, itemCreatedAtColumn, itemStatusColumn, itemUpdatedAtColumn, routerLinkActionButton, textColumn, viewItemActionButton } from '@component/item-list-template/item-list-template.component';
 import { SpinnerDefaultComponent } from '@component/spinner-default/spinner-default.component';
@@ -20,8 +20,63 @@ export class ContractVehicleListPageComponent {
     private fetch = inject(FetchService);
     private activatedRoute = inject(ActivatedRoute);
     public contractPlans = signal<ContractPlan[]>([]);
-    public configurationList?: ItemListConfiguration<ContractPlanVehicle>;
     public contractPlanIdActive = signal<null | number>(null)
+    public configurationList: ItemListConfiguration<ContractPlanVehicle> = {
+        title: 'Vehículos',
+        createButton: {
+            text: 'Instalar vehículos',
+            routerLink: {
+                url: '../installation/create'
+            }
+        },
+        server: {
+            url: 'contract-plan-vehicle',
+            queryParams: {
+                relations: 'contractPlan.plan,vehicle.vehicleType',
+                contract_id: this.activatedRoute.snapshot.parent?.parent?.paramMap.get('id'),
+            },
+        },
+        columns: signal([
+            textColumn({
+                title: 'Placa / Tipo',
+                displayValueFn: (item) => `${item.vehicle?.plate} / ${item.vehicle?.vehicle_type?.name.toUpperCase()}`,
+                gridColumn: '1fr',
+            }),
+            textColumn({
+                title: 'Plan',
+                displayValueFn: (item) => item.contract_plan?.plan?.name ?? '--',
+            }),
+            textColumn({
+                title: 'Color',
+                sort: { key: 'color' },
+                displayValueFn: (item) => item.vehicle?.color ?? '--',
+            }),
+            textColumn({
+                title: 'Marca',
+                sort: { key: 'brand' },
+                displayValueFn: (item) => item.vehicle?.brand ?? '--',
+            }),
+            textColumn({
+                title: 'Modelo',
+                sort: { key: 'model' },
+                displayValueFn: (item) => item.vehicle?.model ?? '--',
+            }),
+            itemCreatedAtColumn(),
+            itemUpdatedAtColumn(),
+            itemStatusColumn(),
+        ]),
+        rows: {
+            options: [
+                routerLinkActionButton({
+                    icon: 'visibility',
+                    text: 'Ver',
+                    routerLink: {
+                        url: (item) => `../view/${item.vehicle?.id}`
+                    },
+                })
+            ]
+        }
+    };
 
     ngOnInit() {
         this.getContractPlans();
@@ -30,74 +85,13 @@ export class ContractVehicleListPageComponent {
     private async getContractPlans() {
         const contractId = this.activatedRoute.snapshot.parent?.parent?.paramMap.get('id');
         const data = (await this.fetch.get<PaginatorData<ContractPlan>>(`contract-plan?relations=plan&contract_id=${contractId}`)).data;
-        if (data.length) {
-            this.setConfigurationList(data[0]);
-            this.contractPlans.set(data);
-        }
+        if (data.length) this.contractPlans.set(data.sort((a, b) => a.plan_id - b.plan_id));
     }
 
-    private setConfigurationList(contractPlan: ContractPlan) {
-        this.contractPlanIdActive.set(contractPlan.id);
-        this.configurationList = {
-            title: 'Vehículos',
-            createButton: {
-                text: 'Instalar vehículos',
-                routerLink: {
-                    url: '../installation/create'
-                }
-            },
-            server: {
-                url: 'contract-plan-vehicle',
-                queryParams: {
-                    relations: 'contractPlan.plan,vehicle.vehicleType',
-                    contract_plan_id: contractPlan.id
-                },
-            },
-            columns: signal([
-                textColumn({
-                    title: 'Placa / Tipo',
-                    displayValueFn: (item) => `${item.vehicle?.plate} / ${item.vehicle?.vehicle_type?.name.toUpperCase()}`,
-                    gridColumn: '1fr',
-                }),
-                textColumn({
-                    title: 'Plan',
-                    displayValueFn: (item) => item.contract_plan?.plan?.name ?? '--',
-                }),
-                textColumn({
-                    title: 'Color',
-                    sort: { key: 'color' },
-                    displayValueFn: (item) => item.vehicle?.color ?? '--',
-                }),
-                textColumn({
-                    title: 'Marca',
-                    sort: { key: 'brand' },
-                    displayValueFn: (item) => item.vehicle?.brand ?? '--',
-                }),
-                textColumn({
-                    title: 'Modelo',
-                    sort: { key: 'model' },
-                    displayValueFn: (item) => item.vehicle?.model ?? '--',
-                }),
-                itemCreatedAtColumn(),
-                itemUpdatedAtColumn(),
-                itemStatusColumn(),
-            ]),
-            rows: {
-                options: [
-                    routerLinkActionButton({
-                        icon: 'visibility',
-                        text: 'Ver',
-                        routerLink: { url: (item) => `../view/${item.vehicle?.id}` },
-                    })
-                ]
-            }
-        }
-    }
-
-    public changeFilterPlan(contractPlan: ContractPlan) {
-        if (contractPlan.id == this.contractPlanIdActive()) return;
-        this.contractPlanIdActive.set(contractPlan.id);
-        this.configurationList!.server!.queryParams!['contract_plan_id'] = contractPlan.id;
+    public changeFilterPlan(contractPlanId: number | null) {
+        if (contractPlanId == this.contractPlanIdActive()) return;
+        this.contractPlanIdActive.set(contractPlanId);
+        this.configurationList!.server!.queryParams!['contract_plan_id'] = this.contractPlanIdActive();
         this.configurationList!.updateListEvent?.emit();
     }
 }
