2a70742b06bfc877f53c8c59b931948bd4b86dec
Milton Marchan <marchanadrian2@gmail.com>
2024-05-15 16:52:27 -0500

    delete payment in list with comment

--- src/app/modules/organization/clients/client-payments/pages/client-payment-list-page/client-payment-list-page.component.ts
+++ src/app/modules/organization/clients/client-payments/pages/client-payment-list-page/client-payment-list-page.component.ts
@@ -1,27 +1,42 @@
-import { Component, ViewEncapsulation, inject, signal } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ViewEncapsulation, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { ItemListConfiguration, ItemListTemplateComponent, dateColumn, itemCreatedAtColumn, itemStatusColumn, itemUpdatedAtColumn, numberColumn, textColumn, titlecaseColumn, userColumn } from '@component/item-list-template/item-list-template.component';
+import { ConfirmDialogData, ConfirmDialogTemplateComponent } from '@component/confirm-dialog-template/confirm-dialog-template.component';
+import { ItemListConfiguration, ItemListTemplateComponent, clickEventActionButton, dateColumn, editItemActionButton, itemCreatedAtColumn, itemStatusColumn, itemUpdatedAtColumn, numberColumn, textColumn, titlecaseColumn, userColumn, viewItemActionButton } from '@component/item-list-template/item-list-template.component';
 import { BoxMovementTypeEnum } from '@interface/boxMovement';
 import { ClientBoxMovement } from '@interface/clientBoxMovement';
+import { FetchService, RequestInitFetch } from '@service/fetch.service';
 
 @Component({
   selector: 'app-client-payment-list-page',
   standalone: true,
-  imports: [ItemListTemplateComponent, RouterLink],
+  imports: [
+    ItemListTemplateComponent, 
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './client-payment-list-page.component.html',
   styleUrl: './client-payment-list-page.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class ClientPaymentListPageComponent {
+  @ViewChild('deletePaymentTemplate', { static: true }) deletePaymentTemplate!: TemplateRef<any>;
   private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
+  private fetch = inject(FetchService);
+  private matDialog = inject(MatDialog);
   public listConfiguration: ItemListConfiguration<ClientBoxMovement> = {
     title: 'Pagos y movimientos',
     server: {
       url: 'client-box-movement',
-      queryParams: { 
-        relations: 'clientBox,boxMovement', 
-        client_id:  this.router.url.includes('/organization/client/view/') ? this.activatedRoute.snapshot.parent?.parent?.paramMap.get('id') : null,
+      queryParams: {
+        relations: 'clientBox,boxMovement',
+        client_id: this.router.url.includes('/organization/client/view/') ? this.activatedRoute.snapshot.parent?.parent?.paramMap.get('id') : null,
       }
     },
     columns: signal([
@@ -31,7 +46,7 @@ export class ClientPaymentListPageComponent {
       }),
       textColumn({
         title: 'Concepto',
-        displayValueFn: (item) => item.concept,
+        displayValueFn: (item) => item.box_movement?.concept,
         routerLinkValue: { url: (item) => `../detail/${item.id}` },
         gridColumn: '1fr',
       }),
@@ -54,6 +69,59 @@ export class ClientPaymentListPageComponent {
       }),
       itemCreatedAtColumn(),
       itemUpdatedAtColumn(),
-    ])
+    ]),
+    rows: {
+      options: [
+        viewItemActionButton(),
+        editItemActionButton(),
+        clickEventActionButton({
+          icon: 'delete',
+          text: 'Eliminar',
+          fn: (item) => this.deletePayment(item),
+        })
+      ]
+    }
+  }
+
+  public form = new FormGroup({
+    delete_comment: new FormControl(''),
+  });
+  get deleteCommentCtrl(): FormControl { return this.form.get('delete_comment')! as FormControl; }
+
+  private confirmDialog(data: ConfirmDialogData): Promise<boolean> {
+    return new Promise((resolve) => {
+      const dialogRef = this.matDialog.open(ConfirmDialogTemplateComponent, { data });
+      dialogRef.afterClosed().subscribe((result) => resolve(result));
+    });
+  }
+
+  private async deletePayment(item: ClientBoxMovement): Promise<ClientBoxMovement | null> {
+    this.deleteCommentCtrl.reset('');
+    const dialogData: ConfirmDialogData = {
+      icon: 'error',
+      title: '¿Está seguro de eliminar el pago?',
+      description: '',
+      templateRef: this.deletePaymentTemplate,
+      confirmButton: { disabled: true },
+    };
+    const subscribe = this.deleteCommentCtrl.valueChanges.subscribe(() => dialogData.confirmButton!.disabled = this.deleteCommentCtrl.invalid);
+    const confirm = await this.confirmDialog(dialogData);
+    subscribe.unsubscribe();
+    if (!confirm) return null;
+    const url = `client-box-movement/delete/${item.id}`;
+    const body = {
+      delete_comment: this.deleteCommentCtrl.value
+    };
+    const request: RequestInitFetch = {
+      confirmDialog: false,
+      toast: {
+        loading: 'Eliminando registro...',
+        success: 'Registro eliminado',
+        error: (error) => 'Error al eliminar pago',
+      }
+    };
+    const response = await this.fetch.put<ClientBoxMovement>(url, body, request);
+    this.listConfiguration.updateListEvent?.emit();
+    return response;
   }
 }
