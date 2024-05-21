44612254beae05f9b5dee87fc6519441ecb3d591
derianmarchan <derianmarchan@gmail.com>
2024-05-21 09:21:11 -0500

    chore: Update status chip labels for 'deshabilitado' status

--- src/app/shared/pipe/auth-roles.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthRolesPipe } from './auth-roles.pipe';
-
-describe('AuthRolesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AuthRolesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
