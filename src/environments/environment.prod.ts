7d671e6783509c78ac06d571b9684aa8792bfcda
derianmarchan <derianmarchan@gmail.com>
2024-09-11 20:14:30 -0500

    Update API and server URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://nuevo-octant-erp-production.up.railway.app/api/v1',
-  PUBLIC_PATH_SERVER: 'https://nuevo-octant-erp-production.up.railway.app/',
+  API_URL: 'https://nuevoservererp.octant.pe/api/v1',
+  PUBLIC_PATH_SERVER: 'https://nuevoservererp.octant.pe/',
   URL_FILES_SERVER: 'https://erp-allendostmen-application.s3.amazonaws.com/',
 };
